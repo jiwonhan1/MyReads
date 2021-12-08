76764cadea66e6c8f69bb5ace4a5357347260a9d
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2021-12-08 15:26:27 -0800

    working on update bookshelf

---
+++ src/components/Book.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const BookDetails = (props) => {
+  const book = props.book;
+  console.log(props.book);
+  return (
+    <React.Fragment>
+      <li>
+        <div className="book">
+          <div className="book-top" style={{ border: "2px solid blue" }}>
+            <div
+              className="book-cover"
+              style={{
+                width: 135,
+                height: 193,
+                border: "2px solid red",
+                backgroundImage: `url(${
+                  book.imageLinks ? book.imageLinks.thumbnail : null
+                })`,
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select
+                onChange={(e) => props.handleBookShelf(book, e.target.value)}
+                value={book.shelf}
+              >
+                <option value="none" disabled>
+                  {" "}
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors}</div>
+          </div>
+        </div>
+      </li>
+    </React.Fragment>
+  );
+};
+
+export default BookDetails;
---
+++ src/components/SearchPage.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BookDetails from "./Book";
+import * as BooksAPI from "../BooksAPI";
+import BooksApp from "../App";
+
+const SearchPage = (props) => {
+  const [query, setQuery] = React.useState("");
+  const [books, setBooks] = React.useState([]);
+
+  React.useEffect(() => {
+    if (props.books) {
+      setBooks(props.books);
+    }
+  }, []);
+
+  const handleUpdateQuery = (query) => {
+    BooksAPI.search(query).then(
+      (books) => (books ? setBooks(books) : setBooks([])),
+      setQuery(query)
+    );
+  };
+
+  const handleBookShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+      .then(() =>
+        shelf !== "none"
+          ? alert(`${book.title} has benn added to your shelf!`)
+          : null
+      )
+      .catch(() => alert("Something went wrong!"));
+  };
+
+  const renderSearchResult = () => {
+    if (query) {
+      return books.error ? (
+        <div>No results Found</div>
+      ) : (
+        books.map((book, index) => {
+          return (
+            <BookDetails
+              key={index}
+              book={book}
+              handleBookShelf={handleBookShelf.bind(this)}
+            />
+          );
+        })
+      );
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(e) => handleUpdateQuery(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">{renderSearchResult()}</ol>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default SearchPage;
